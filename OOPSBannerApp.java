494b00e4a4f5c08bc83f21957d9ac35df2d446d8
Dhara <dharaniviswanadh@example.com>
2026-02-16 23:44:22 +0530

    UC2: Display OOPS as ASCII banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,21 @@
 /**
  * OOPSBannerApp
  *
- * Prints literal text "OOPS" to console.
+ * Displays "OOPS" as a large ASCII banner using '*' characters.
  *
  * @author Dhara
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *   ***** ");
+        System.out.println("*     *  *     *  *     *        *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println(" *****    *****    *****    ***** ");
     }
 }
